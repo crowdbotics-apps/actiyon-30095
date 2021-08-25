afefaa5cf08c23245ac8accaa310f9d1939a7ca4
Crowdbotics <team@crowdbotics.com>
2021-08-25 11:10:42 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserLang
+
+admin.site.register(UserLang)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,15 @@
+from django.conf import settings
 from django.db import models
+
+
+class UserLang(models.Model):
+    "Generated Model"
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="userlang_user",
+    )
+    language = models.CharField(
+        max_length=256,
+    )
+    level = models.BigIntegerField()
